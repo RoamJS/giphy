0825374fb3b7095ab9649bad0d458d5ab742db37
David Vargas <dvargas92495@gmail.com>
2022-10-22 23:43:01 -0400

    Publish to Roam Depot

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# giphy
-        
-Description for giphy.
-  
-For full documentation, checkout https://roamjs.com/extensions/giphy!
+# Giphy
+
+Quickly insert GIFs from Giphy into Roam!
+
+## Usage
+
+Type `{{GIPHY: }}` into a block. Continue typing your gif search term after the colon. A list of GIPHY results will render below the block as you type. Click on one to replace the text with a link to the gif.
+
+For example, typing `{{GIPHY:jumping cat}}` and clicking on the first result will render:
+
+![Loop Jump GIF by sheepfilms](https://media2.giphy.com/media/gKfNj8cYeGN63bLRkF/giphy.gif?cid=d3ed119egqzsfvzi63mxlmvcs8qrd9cr5jdx40p39idxvazc&rid=giphy.gif)
+
+## Demo
+
+<video src="https://firebasestorage.googleapis.com/v0/b/firescript-577a2.appspot.com/o/imgs%2Fapp%2Froamjs%2Fz3FiE3zwIl.mp4?alt=media&token=c6d39719-d305-47ba-9716-177d201d5ac8" controls="controls" height="600"></video>
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import runExtension from "roamjs-components/util/runExtension";
+import createHTMLObserver from "roamjs-components/dom/createHTMLObserver";
+import { render } from "./components/GiphyPopover";
 
 export default runExtension({
-  run: (args) => {
-    args.extensionAPI.settings.panel.create({
-      tabTitle: "giphy",
-      settings: [],
+  run: () => {
+    // no settings yet
+    // args.extensionAPI.settings.panel.create({
+    //   tabTitle: "giphy",
+    //   settings: [],
+    // });
+
+    createHTMLObserver({
+      tag: "TEXTAREA",
+      className: "rm-block-input",
+      callback: (t: HTMLTextAreaElement) => {
+        render(t);
+      },
+      removeCallback: () =>
+        Array.from(
+          document.getElementsByClassName("roamjs-giphy-portal")
+        ).forEach((p) =>
+          p.parentElement.parentElement.removeChild(p.parentElement)
+        ),
     });
   },
 });
