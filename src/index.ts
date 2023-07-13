f2989cee76a95eed741e35677f691b33fdcdec6c
Michael Gartner <mclicks@gmail.com>
2023-07-13 11:33:28 -0700

    update roamjs-components to 0.80.3

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,24 @@ import runExtension from "roamjs-components/util/runExtension";
 import createHTMLObserver from "roamjs-components/dom/createHTMLObserver";
 import { render } from "./components/GiphyPopover";
 
-export default runExtension({
-  run: () => {
-    // no settings yet
-    // args.extensionAPI.settings.panel.create({
-    //   tabTitle: "giphy",
-    //   settings: [],
-    // });
+export default runExtension(async () => {
+  // no settings yet
+  // args.extensionAPI.settings.panel.create({
+  //   tabTitle: "giphy",
+  //   settings: [],
+  // });
 
-    createHTMLObserver({
-      tag: "TEXTAREA",
-      className: "rm-block-input",
-      callback: (t: HTMLTextAreaElement) => {
-        render(t);
-      },
-      removeCallback: () =>
-        Array.from(
-          document.getElementsByClassName("roamjs-giphy-portal")
-        ).forEach((p) =>
-          p.parentElement.parentElement.removeChild(p.parentElement)
-        ),
-    });
-  },
+  createHTMLObserver({
+    tag: "TEXTAREA",
+    className: "rm-block-input",
+    callback: (t: HTMLElement) => {
+      render(t as HTMLTextAreaElement);
+    },
+    removeCallback: () =>
+      Array.from(
+        document.getElementsByClassName("roamjs-giphy-portal")
+      ).forEach((p) =>
+        p.parentElement?.parentElement?.removeChild(p.parentElement)
+      ),
+  });
 });
